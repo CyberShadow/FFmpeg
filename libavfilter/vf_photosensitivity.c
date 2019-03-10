7ef1a8558fa5c31a2e95a4c388889e6f231f7c0d
Vladimir Panteleev <git@thecybershadow.net>
2019-03-10 11:38:18 +0000

    libavfilter/vf_photosensitivity.c: Calculate/track badness and blend per-pixel

--- libavfilter/vf_photosensitivity.c
+++ libavfilter/vf_photosensitivity.c
@@ -30,12 +30,7 @@
 #include "video.h"
 
 #define MAX_FRAMES 240
-#define GRID_SIZE 8
 #define NUM_CHANNELS 3
-
-typedef struct PhotosensitivityFrame {
-    uint8_t grid[GRID_SIZE][GRID_SIZE][4];
-} PhotosensitivityFrame;
 
 typedef struct PhotosensitivityContext {
     const AVClass *class;
@@ -48,19 +43,18 @@ typedef struct PhotosensitivityContext {
     int badness_threshold;
 
     /* Circular buffer */
-    int history[MAX_FRAMES];
+    uint32_t *history[MAX_FRAMES];
     int history_pos;
 
-    PhotosensitivityFrame last_frame_e;
-    AVFrame *last_frame_av;
+    AVFrame *last_frame;
 } PhotosensitivityContext;
 
 #define OFFSET(x) offsetof(PhotosensitivityContext, x)
 #define FLAGS AV_OPT_FLAG_VIDEO_PARAM|AV_OPT_FLAG_FILTERING_PARAM
 
 static const AVOption photosensitivity_options[] = {
-    { "frames",    "set how many frames to use"                        ,  OFFSET(nb_frames           ), AV_OPT_TYPE_INT  , {.i64=30}, 2, MAX_FRAMES, FLAGS },
-    { "f",         "set how many frames to use"                        ,  OFFSET(nb_frames           ), AV_OPT_TYPE_INT  , {.i64=30}, 2, MAX_FRAMES, FLAGS },
+    { "frames",    "set how many frames to use"                        ,  OFFSET(nb_frames           ), AV_OPT_TYPE_INT  , {.i64=60}, 2, MAX_FRAMES, FLAGS },
+    { "f",         "set how many frames to use"                        ,  OFFSET(nb_frames           ), AV_OPT_TYPE_INT  , {.i64=60}, 2, MAX_FRAMES, FLAGS },
     { "threshold", "set detection threshold factor (lower is stricter)",  OFFSET(threshold_multiplier), AV_OPT_TYPE_FLOAT, {.dbl= 1}, 0, FLT_MAX   , FLAGS },
     { "t"        , "set detection threshold factor (lower is stricter)",  OFFSET(threshold_multiplier), AV_OPT_TYPE_FLOAT, {.dbl= 1}, 0, FLT_MAX   , FLAGS },
     { "skip"     , "set pixels to skip when sampling frames"           ,  OFFSET(skip                ), AV_OPT_TYPE_INT  , {.i64= 1}, 1, 1024      , FLAGS },
@@ -83,157 +77,87 @@ static int query_formats(AVFilterContext *ctx)
     return ff_set_common_formats(ctx, formats);
 }
 
-typedef struct ThreadData_convert_frame
+typedef struct ThreadData_process_frame
 {
-    AVFrame *in;
-    PhotosensitivityFrame* out;
-    int skip;
-} ThreadData_convert_frame;
+    PhotosensitivityContext *s;
+    AVFrame *in, *out;
+} ThreadData_process_frame;
 
-#define NUM_CELLS (GRID_SIZE * GRID_SIZE)
-
-static int convert_frame_partial(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
+static int process_frame_partial(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
 {
-    int cell, gx, gy, x0, x1, y0, y1, x, y, c, area;
-    int sum[NUM_CHANNELS];
-    const uint8_t *p;
+    int x, y, p, c, i, this_badness, current_badness, new_badness;
+    float factor;
+    uint32_t *history;
+    uint8_t *p_in, *p_out, *p_last;
 
-    ThreadData_convert_frame *td = arg;
+    PhotosensitivityContext *s = ctx->priv;
+    ThreadData_process_frame *td = arg;
+    const int slice_start = (td->out->height * jobnr) / nb_jobs;
+    const int slice_end = (td->out->height * (jobnr+1)) / nb_jobs;
+    const int width = td->out->width;
 
-    const int slice_start = (NUM_CELLS * jobnr) / nb_jobs;
-    const int slice_end = (NUM_CELLS * (jobnr+1)) / nb_jobs;
-
-    int width = td->in->width, height = td->in->height, linesize = td->in->linesize[0], skip = td->skip;
-    const uint8_t *data = td->in->data[0];
-
-    for (cell = slice_start; cell < slice_end; cell++) {
-        gx = cell % GRID_SIZE;
-        gy = cell / GRID_SIZE;
-
-        x0 = width  *  gx    / GRID_SIZE;
-        x1 = width  * (gx+1) / GRID_SIZE;
-        y0 = height *  gy    / GRID_SIZE;
-        y1 = height * (gy+1) / GRID_SIZE;
-
-        for (c = 0; c < NUM_CHANNELS; c++) {
-            sum[c] = 0;
-        }
-        for (y = y0; y < y1; y += skip) {
-            p = data + y * linesize + x0 * NUM_CHANNELS;
-            for (x = x0; x < x1; x += skip) {
-                //av_log(NULL, AV_LOG_VERBOSE, "%d %d %d : (%d,%d) (%d,%d) -> %d,%d | *%d\n", c, gx, gy, x0, y0, x1, y1, x, y, (int)row);
-                sum[0] += p[0];
-                sum[1] += p[1];
-                sum[2] += p[2];
-                p += NUM_CHANNELS * skip;
-                // TODO: variable size
-            }
-        }
-
-        area = ((x1 - x0 + skip - 1) / skip) * ((y1 - y0 + skip - 1) / skip);
-        for (c = 0; c < NUM_CHANNELS; c++) {
-            if (area)
-                sum[c] /= area;
-            td->out->grid[gy][gx][c] = sum[c];
-        }
-    }
-    return 0;
-}
-
-static void convert_frame(AVFilterContext *ctx, AVFrame *in, PhotosensitivityFrame* out, int skip)
-{
-    ThreadData_convert_frame td;
-    td.in = in;
-    td.out = out;
-    td.skip = skip;
-    ctx->internal->execute(ctx, convert_frame_partial, &td, NULL, FFMIN(NUM_CELLS, ff_filter_get_nb_threads(ctx)));
-}
-
-typedef struct ThreadData_blend_frame
-{
-    AVFrame *target;
-    AVFrame *source;
-    uint16_t s_mul;
-} ThreadData_blend_frame;
-
-static int blend_frame_partial(AVFilterContext *ctx, void *arg, int jobnr, int nb_jobs)
-{
-    int x, y;
-    uint8_t *t, *s;
-
-    ThreadData_blend_frame *td = arg;
-    const uint16_t s_mul = td->s_mul;
-    const uint16_t t_mul = 0x100 - s_mul;
-    const int slice_start = (td->target->height * jobnr) / nb_jobs;
-    const int slice_end = (td->target->height * (jobnr+1)) / nb_jobs;
-    const int linesize = td->target->linesize[0];
-
+    p = slice_start * td->in->width;
     for (y = slice_start; y < slice_end; y++) {
-        t = td->target->data[0] + y * td->target->linesize[0];
-        s = td->source->data[0] + y * td->source->linesize[0];
-        for (x = 0; x < linesize; x++) {
-            *t = (*t * t_mul + *s * s_mul) >> 8;
-            t++; s++;
+        p_in   = &td->in       ->data[0][y * td->in       ->linesize[0]];
+        p_out  = &td->out      ->data[0][y * td->out      ->linesize[0]];
+        p_last = &s->last_frame->data[0][y * s->last_frame->linesize[0]];
+
+        for (x = 0; x < width; x++) {
+            this_badness = 0;
+            for (c = 0; c < NUM_CHANNELS; c++)
+                this_badness += abs((int)p_last[c] - (int)p_in[c]);
+
+            current_badness = 0;
+            for (i = 1; i < s->nb_frames; i++) {
+                history = s->history[(s->history_pos + i) % s->nb_frames];
+                if (history)
+                    current_badness += i * history[p];
+            }
+            current_badness /= s->nb_frames;
+            new_badness = current_badness + this_badness;
+
+            if (new_badness < s->badness_threshold || !s->last_frame || s->bypass) {
+                for (c = 0; c < NUM_CHANNELS; c++)
+                    p_out[c] = p_in[c];
+            } else {
+                factor = (float)(s->badness_threshold - current_badness) / (new_badness - current_badness);
+                if (factor <= 0) {
+                    /* just duplicate the frame */
+                    for (c = 0; c < NUM_CHANNELS; c++)
+                        p_out[c] = p_last[c];
+                    this_badness = 0; /* frame was duplicated, thus, delta is zero */
+                } else {
+                    for (c = 0; c < NUM_CHANNELS; c++)
+                        p_out[c] = (uint8_t)(p_in[c] * factor + p_last[c] * (1-factor));
+
+                    this_badness = 0;
+                    for (c = 0; c < NUM_CHANNELS; c++)
+                        this_badness += abs((int)p_last[c] - (int)p_out[c]);
+                    /* new_badness = current_badness + this_badness; */
+                }
+            }
+            s->history[s->history_pos][p] = this_badness;
+
+            p++;
+            p_in   += NUM_CHANNELS;
+            p_out  += NUM_CHANNELS;
+            p_last += NUM_CHANNELS;
         }
     }
     return 0;
-}
-
-static void blend_frame(AVFilterContext *ctx, AVFrame *target, AVFrame *source, float factor)
-{
-    ThreadData_blend_frame td;
-    td.target = target;
-    td.source = source;
-    td.s_mul = (uint16_t)(factor * 0x100);
-    ctx->internal->execute(ctx, blend_frame_partial, &td, NULL, FFMIN(ctx->outputs[0]->h, ff_filter_get_nb_threads(ctx)));
-}
-
-static int get_badness(PhotosensitivityFrame* a, PhotosensitivityFrame* b)
-{
-    int badness, x, y, c;
-    badness = 0;
-    for (c = 0; c < NUM_CHANNELS; c++) {
-        for (y = 0; y < GRID_SIZE; y++) {
-            for (x = 0; x < GRID_SIZE; x++) {
-                badness += abs((int)a->grid[y][x][c] - (int)b->grid[y][x][c]);
-                //av_log(NULL, AV_LOG_VERBOSE, "%d - %d -> %d \n", a->grid[y][x], b->grid[y][x], badness);
-                //av_log(NULL, AV_LOG_VERBOSE, "%d -> %d \n", abs((int)a->grid[y][x] - (int)b->grid[y][x]), badness);
-            }
-        }
-    }
-    return badness;
 }
 
 static int config_input(AVFilterLink *inlink)
 {
-    /* const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(inlink->format); */
     AVFilterContext *ctx = inlink->dst;
     PhotosensitivityContext *s = ctx->priv;
 
-    s->badness_threshold = (int)(GRID_SIZE * GRID_SIZE * 4 * 256 * s->nb_frames * s->threshold_multiplier / 128);
+    s->badness_threshold = (int)(4 * 256 * s->nb_frames * s->threshold_multiplier / 64);
 
     return 0;
 }
 
-static int filter_frame(AVFilterLink *inlink, AVFrame *in)
-{
-    int this_badness, current_badness, new_badness, i, res;
-    PhotosensitivityFrame ef;
-    AVFrame *src, *out;
-    float factor;
-    AVDictionary **metadata;
-
-    AVFilterContext *ctx = inlink->dst;
-    AVFilterLink *outlink = ctx->outputs[0];
-    PhotosensitivityContext *s = ctx->priv;
-
-    /* weighted moving average */
-    current_badness = 0;
-    for (i = 1; i < s->nb_frames; i++)
-        current_badness += i * s->history[(s->history_pos + i) % s->nb_frames];
-    current_badness /= s->nb_frames;
-
-    convert_frame(ctx, in, &ef, s->skip);
+#if 0
     this_badness = get_badness(&ef, &s->last_frame_e);
     new_badness = current_badness + this_badness;
     av_log(s, AV_LOG_VERBOSE, "badness: %6d -> %6d / %6d (%3d%% - %s)                                                     \n",
@@ -242,10 +166,6 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
 
     if (new_badness < s->badness_threshold || !s->last_frame_av || s->bypass) {
         factor = 1; /* for metadata */
-        if (s->last_frame_av) {
-            av_frame_free(&s->last_frame_av);
-        }
-        s->last_frame_av = src = in;
         s->last_frame_e = ef;
         s->history[s->history_pos] = this_badness;
     } else {
@@ -272,7 +192,24 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         }
         src = s->last_frame_av;
     }
-    s->history_pos = (s->history_pos + 1) % s->nb_frames;
+#endif
+
+static int filter_frame(AVFilterLink *inlink, AVFrame *in)
+{
+    AVFrame *out;
+    ThreadData_process_frame td;
+
+    AVFilterContext *ctx = inlink->dst;
+    AVFilterLink *outlink = ctx->outputs[0];
+    PhotosensitivityContext *s = ctx->priv;
+
+    if (!s->history[s->history_pos]) {
+        s->history[s->history_pos] = malloc(in->width * in->height * sizeof(s->history[0][0]));
+        if (!s->history[s->history_pos]) {
+            av_frame_free(&in);
+            return AVERROR(ENOMEM);
+        }
+    }
 
     out = ff_get_video_buffer(outlink, in->width, in->height);
     if (!out) {
@@ -280,17 +217,19 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *in)
         return AVERROR(ENOMEM);
     }
     av_frame_copy_props(out, in);
-    metadata = &out->metadata;
-    if (metadata) {
-        char value[128];
 
-        snprintf(value, sizeof(value), "%f", (float)new_badness / s->badness_threshold);
-        av_dict_set(metadata, "lavfi.photosensitivity.badness", value, 0);
+    if (!s->last_frame)
+        s->last_frame = av_frame_clone(in);
 
-        snprintf(value, sizeof(value), "%f", factor);
-        av_dict_set(metadata, "lavfi.photosensitivity.factor", value, 0);
-    }
-    av_frame_copy(out, src);
+    td.in = in;
+    td.out = out;
+    ctx->internal->execute(ctx, process_frame_partial, &td, NULL,
+        FFMIN(ctx->outputs[0]->h, ff_filter_get_nb_threads(ctx)));
+
+    av_frame_unref(s->last_frame);
+    av_frame_ref(s->last_frame, out);
+    s->history_pos = (s->history_pos + 1) % s->nb_frames;
+
     return ff_filter_frame(outlink, out);
 }
 
@@ -298,9 +237,10 @@ static av_cold void uninit(AVFilterContext *ctx)
 {
     PhotosensitivityContext *s = ctx->priv;
 
-    if (s->last_frame_av) {
-        av_frame_free(&s->last_frame_av);
+    if (s->last_frame) {
+        av_frame_free(&s->last_frame);
     }
+    /* TODO free history */
 }
 
 static const AVFilterPad inputs[] = {
